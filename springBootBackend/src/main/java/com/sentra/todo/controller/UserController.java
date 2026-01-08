c59f50ba01723d3a51e7bc6f900bea05731ac6ee
Nitin Rana <nitinranagraphian@gmail.com>
2026-01-08 22:06:30 +0530

    Add UserController and UserRepository for user management functionality

---
+++ springBootBackend/src/main/java/com/sentra/todo/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.sentra.todo.controller;
+
+import com.sentra.todo.entity.User;
+import com.sentra.todo.repository.UserRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    
+    private final UserRepository userRepository;
+    
+    @PostMapping
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+    
+    @GetMapping
+    public ResponseEntity<Page<User>> getUsers(Pageable pageable) {
+        Page<User> users = userRepository.findAll(pageable);
+        return ResponseEntity.ok(users);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> ResponseEntity.ok(user))
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
